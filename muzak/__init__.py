06ad90349da6de2cfcf972296b0d0bb3d30cfe4f
Gage LeBlanc <gleblanc@symnet.io>
2022-05-24 21:36:40 -0400

    version bump

--- muzak/__init__.py
+++ muzak/__init__.py
@@ -14,7 +14,7 @@ from clilib.config.config_loader import JSONConfigurationFile
 from muzak.drivers import MuzakQueryResult, MuzakStorageDriver
 
 
-__version__ = "0.8.3"
+__version__ = "0.8.4"
 
 
 default_config = {
